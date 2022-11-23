bf2209be8ba728ccfa3cf75d07d38d8e7df6fcfc
Shekhar Thakur <connectshekharthakur09@gmail.com>
2022-11-23 19:25:27 +0530

    Fetching Todos via API and Logging them to console!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import {useEffect } from "react";
 import { Navbar } from "./components/Navbar";
 import AddTodoForm from "./components/AddTodoForm";
 
 function App() {
+
+  useEffect(() => {
+    getTodos();
+  })
+
+  function getTodos() {
+    fetch('https://jsonplaceholder.typicode.com/todos').then((result) => {
+      result.json().then((res) => {
+        console.log(res)
+      })
+    })
+  }
+
   return (
     <div className="App">
-        <Navbar/>
-        <AddTodoForm/>
+      <Navbar />
+      <AddTodoForm />
     </div>
   );
 }
