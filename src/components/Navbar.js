e5aa2ff1d18156f8f4504b80ddc948888006776f
Shekhar Thakur <connectshekharthakur09@gmail.com>
2022-11-23 18:50:01 +0530

    Created the Navbar Component + Styled it !

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        // Navbar Component 
+        <header className='header'>
+            <img src='https://cdn-icons-png.flaticon.com/512/4697/4697260.png' alt="Navbar Logo" />
+            <h1 className='header-title'>TODO-NATION.</h1>
+        </header>
+    )
+}
